7679a7ba3f34552c766bb062748e7d82b06a389a
Mykhailo Hodis <godismichael1995@gmail.com>
2017-07-28 18:20:43 +0300

    - Updated script for dataset creation

--- img_converter.py
+++ img_converter.py
@@ -1,70 +1,121 @@
+
 import cv2
-import numpy as np
+import progressbar
+import subprocess as sp
 import argparse
 import os
 
-#https://habrahabr.ru/post/144416/
-parser = argparse.ArgumentParser(description='Positive/negative images preparation for HAAR cascade')
-subpasers = parser.add_subparsers(help='List of commands')
 
-parser.add_argument('-p', '--path', required=True, help='Path to your images')
-parser.add_argument('-t', '--type', required=True, help='Specify whether positive or negative images are processed (type "pos" or "neg")')
-parser.add_argument('-wd', '--width', required=False, type = int, help='Image width (default: positive - 60 | negative - 500)')
-parser.add_argument('-hg', '--height', required=False, type = int, help='Image height (default: positive - 90 | negative - 600)')
+parser = argparse.ArgumentParser(description='Easy dataset creation for HAAR cascades v0.1')
+subpasers = parser.add_subparsers(dest='command', help='List of commands')
+
+image_parser = subpasers.add_parser('prepareimages', help='Crop and rescale positive/negative images')
+image_parser.add_argument('-p', '--path', required=True, help='Path to your images')
+image_parser.add_argument('-t', '--type', required=True, help='Specify whether positive or negative images are '
+                                                               'processed (type "pos" or "neg")')
+image_parser.add_argument('-wd', '--width', required=False, type=int, help='Image width (default: positive - 60 | '
+                                                                            'negative - 500)')
+image_parser.add_argument('-hg', '--height', required=False, type=int, help='Image height (default: positive - 90 | '
+                                                                             'negative - 600)')
+
+description_parser = subpasers.add_parser('createdescriptions', help='Create description files for positive/negative images')
+description_parser.add_argument('-p', '--path', required=True, help='Path to folder with negative images')
+description_parser.add_argument('-t', '--type', required=True, help='Specify whether positive or negative images are '
+                                                             'processed (type "pos" or "neg")')
+description_parser.add_argument('-wd', '--width', required=False, type=int, help='Positive image width ('
+                                                                                             'default: 60)')
+description_parser.add_argument('-hg', '--height', required=False, type=int, help='>Positive image height '
+                                                                                              '(default: 90)')
+description_parser.add_argument('-f', '--format', required=False, default='txt', help='Specify format of file')
+
+info_parser = subpasers.add_parser('createinfo', help='Create info images and info text file')
+info_parser.add_argument('-p', '--path', required = True, help='Path to positive images folder')
+info_parser.add_argument('-bg', '--background', required=True, help='Description file for negative images')
+info_parser.add_argument('-o', '--output', required=True, help='Output folder for info files')
+info_parser.add_argument('-mx', '--maxX', required=False, default=0, type=float, help='Max X angle (default: 0)')
+info_parser.add_argument('-my', '--maxY', required=False, default=0, type=float, help='Max Y angle (default: 0)')
+info_parser.add_argument('-mz', '--maxZ', required=False, default=0, type=float, help='Max Z angle (default: 0)')
+info_parser.add_argument('-n', '--num', required=False, default=200, type=int, help='Number of objects fot each positive image (default: 200)')
+info_parser.add_argument('-wd', '--width', required=False, default=20, type=int, help='Ratio width (default: 20))')
+info_parser.add_argument('-hg', '--height', required=False, default=20, type=int, help='Ratio height (default: 20)')
+info_parser.add_argument('-tr', '--threshold', required=False, default=0, type=int, help='Background threshold (default: 0)')
 
 args = parser.parse_args()
 
-
-def store_raw_images(imgs_path, imgs_type):
-
-    imgs_format = '.jpg'
-
-    if imgs_type == 'pos':
+if args.command in ('prepareimages', 'createdescriptions'):
+    if args.type == 'pos':
         width = args.width if args.width else 60
         height = args.height if args.height else 90
-    elif imgs_type == 'neg':
-        width = int(args.width) if args.width else 500
-        height = int(args.height) if args.height else 600
+    elif args.type == 'neg':
+        width = args.width if args.width else 500
+        height = args.height if args.height else 600
+    else:
+        raise(TypeError("Type should be 'pos' or 'neg'. Usage example:\n"
+                        "user: python3 img_converter.py list -p positives/ -t pos"))
 
-    # print(width, height)
+# Progress bar
+bar = progressbar.ProgressBar()
 
-    if any([img[0:3] == imgs_type for img in os.listdir(imgs_path)]):
-        current_imgs = list(filter(lambda x: x[0:3] == imgs_type, os.listdir(imgs_path)))
+def store_raw_images(path, type):
+    imgs_format = '.jpg'
+
+    if any([img[0:3] == type for img in os.listdir(path)]):
+        current_imgs = list(filter(lambda x: x[0:3] == type, os.listdir(path)))
         name_index = max(list(map(lambda x: int(x[4:-4]), current_imgs))) + 1
-        imgs = list(filter(lambda x: x[0:3] != imgs_type, os.listdir(imgs_path)))
+        imgs = list(filter(lambda x: x[0:3] != type, os.listdir(path)))
     else:
         name_index = 1
-        imgs = os.listdir(imgs_path)
+        imgs = os.listdir(path)
 
-    for img in imgs:
+    for img in bar(imgs):
         try:
             # Grayscaling and resizing
-            grayscaled = cv2.imread(imgs_path + img, cv2.IMREAD_GRAYSCALE)
+            grayscaled = cv2.imread(path + img, cv2.IMREAD_GRAYSCALE)
             resized = cv2.resize(grayscaled, (width, height))
-            cv2.imwrite(imgs_path + imgs_type + '-' + str(name_index) + imgs_format, resized)
+            cv2.imwrite(path + type + '-' + str(name_index) + imgs_format, resized)
             name_index += 1
 
             # Deleting origin image
-            os.remove(imgs_path + img)
+            os.remove(path + img)
 
         except Exception as e:
-            os.remove(imgs_path + img)
+            os.remove(path + img)
 
 
-store_raw_images(args.path, args.type)
-
-
-
-def create_descriptions(path):
-    for img in os.listdir(path):
-        if path[0:-1] == 'neg':
+def create_descriptions(path, type):
+    if type == 'neg':
+        if 'bg.' + args.format in os.listdir('.'):
+            os.remove('bg.' + args.format)
+        for img in bar(os.listdir(path)):
             line = path + img + '\n'
-            with open('bg.txt', 'a') as f:
+            with open('bg.' + args.format, 'a') as f:
                 f.write(line)
 
-        elif path[0:-1] == 'pos':
-            line = path + img + ' 1 0 0 80 120\n'
-            with open('info.dat', 'a') as f:
+    elif type == 'pos':
+        if 'info.' + args.format in os.listdir('.'):
+            os.remove('info.' + args.format)
+        for img in bar(os.listdir(path)):
+            line = path + img + ' 1 0 0 ' + str(width) + ' ' + str(height) + '\n'
+            with open('info.' + args.format, 'a') as f:
                 f.write(line)
 
-# create_pos_n_neg('neg/')
+def create_info(path, background, output, maxX, maxY, maxZ, number, width, height, threshold):
+    info_index = 1
+
+    for img in bar(os.listdir(path)):
+        path_to_image = path + img
+        info_name = 'info'+str(info_index)
+        info_output = output + info_name
+
+        sp.run(
+            ['opencv_createsamples', '-img', path_to_image, '-bg', background, '-info', info_output, '-pngoutput',
+             'info', '-maxxangle', maxX, '-maxyangle', maxY, '-maxzangle', maxZ, '-num', number, '-w', width,
+             '-h', height, '-bgthresh', threshold])
+
+
+if args.command == 'prepareimages':
+    store_raw_images(args.path, args.type)
+elif args.command == 'createdescriptions':
+    create_descriptions(args.path, args.type)
+elif args.command == 'createinfo':
+    create_info(args.path, args.background, args.output, args.maxX, args.maxY, args.maxZ, args.num, args.width, args.height, args.threshold)
